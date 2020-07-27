e2507fce7f64615c04c1ac9ed94999d2b02dae74
LakshmiSai <lakshmi.peddi@zensar.com>
2020-07-27 21:48:30 +0530

    Initial commit

---
+++ html/scripts/page.js
@@ -0,0 +1,45 @@
+let btn = document.getElementById('btnSubmit');
+
+function validateForm() {
+    var error = 0;
+    var name = document.getElementById('signupName').value;
+    document.getElementById('name_error').innerHTML = '';
+    if (name == null || name == "" || name.length < 3) {
+        error++;
+        document.getElementById('name_error').innerHTML = 'Username must be at least 3 characters';
+    }
+
+    var email = document.getElementById('signupEmail').value;
+    document.getElementById('email_error').innerHTML = '';
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        
+       error = 0;
+    }
+    else{
+         error++;
+        document.getElementById('email_error').innerHTML = 'Email is not valid';
+    }
+
+    var pass = document.getElementById('signupPassword').value;
+    document.getElementById('pass_error').innerHTML = '';
+    if (pass == null || pass == ""|| pass.length < 6) {
+        error++;
+        document.getElementById('pass_error').innerHTML = 'Password must be atleast 6 characters';
+    }
+
+    // var pass_again = document.getElementById('signupPassword').value;
+    // document.getElementById('pass_again_error').innerHTML = '';
+    // if (pass == null || pass == ""){
+    //     error++;
+    //     document.getElementById('pass_error').innerHTML = 'Password2 is required';
+    // }
+    else(pass_again_error !== pass) {
+        error++;
+        document.getElementById('pass_again_error').innerHTML = 'Password2 is required';
+    }
+
+    if(error>0) {
+        return false;
+    }
+    return true;
+}
